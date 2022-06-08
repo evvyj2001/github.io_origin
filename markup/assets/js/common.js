4d952cdc12214492e2b77bd608fd0013c08df610
yjsung <yjsung@da-rum.com>
2022-06-08 18:30:55 +0900

    no message

--- markup/assets/js/common.js
+++ markup/assets/js/common.js
@@ -1,3 +1,4 @@
+// screen height
 function setScreenHeight() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -32,6 +33,44 @@ function navBottom() {
     });
 }
 
+// header mobile nav
+function mobileNav() {
+    const navBtn = document.querySelector('.nav-btn');
+    const navWrap = document.querySelector('.nav-wrap');
+    const screenSize = document.body.clientWidth;
+    navBtn.addEventListener('click', () => {
+        if (!navBtn.classList.contains('is-on')) {
+            navBtn.classList.add('is-on');
+            navWrap.style.transform = `translateX(0)`;
+        } else {
+            navBtn.classList.remove('is-on');
+            navWrap.style.transform = `translateX(100%)`;
+        }
+    });
+
+    if (screenSize > 690) {
+        navBtn.classList.remove('is-on');
+        navWrap.style.transform = `translateX(100%)`;
+    }
+}
+
+// header mobile nav menu
+function mobileNavMenu() {
+    const navList = document.querySelectorAll('.nav-wrap > ul > li');
+
+    navList.forEach(el => {
+        const item = el.querySelector('a');
+
+        item.addEventListener('click', elm => {
+            for (let i = 0; i < navList.length; i++) {
+                navList[i].classList.remove('is-on');
+            }
+
+            elm.target.parentNode.classList.add('is-on');
+        });
+    });
+}
+
 // header date
 function isNow() {
     const dateElem = document.querySelector('.now .date');
@@ -42,7 +81,7 @@ function isNow() {
     const month = now.getMonth() + 1;
     const date = now.getDate();
     const day = now.getDay() - 1;
-    const week = ['월', '화', '수', '목', '금', '토', '일'];
+    const week = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
 
     // const hours = String(now.getHours()).padStart(2, '0');
     const hours = String(now.getHours());
@@ -52,29 +91,63 @@ function isNow() {
 
     if (hours > 12) {
         hour = hours - 12;
-        dayNight.innerText = '오후';
+        dayNight.innerText = 'PM';
     } else {
         hour = hours;
-        dayNight.innerText = '오전';
+        dayNight.innerText = 'AM';
     }
 
-    dateElem.innerText = `${year}년 ${month}월 ${date}일 ${week[day]}요일`;
-    clockElem.innerText = `${hour}시 ${minutes}분 ${seconds}초`;
+    dateElem.innerText = `${year}. ${month}. ${date} ${week[day]}`;
+    clockElem.innerText = `${hour}:${minutes}:${seconds}`;
 }
 
 setInterval(() => {
     isNow();
 }, 1000);
 
-// 로드 즉시 실행
-document.addEventListener('DOMContentLoaded', () => {
+function scrollIndicator() {
+    const winScroll = document.querySelector('.container').scrollTop;
+    const height =
+        document.querySelector('.container').scrollHeight - document.querySelector('.container').clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector('.scroll-indicator > span').style.width = scrolled + '%';
+}
+
+function popOpen(el) {
+    const popDimmed = document.createElement('div');
+    popDimmed.classList.add('popup-dimmed');
+    const popupId = document.getElementById(`${el}`);
+    popupId.before(popDimmed);
+    setTimeout(() => {
+        popDimmed.classList.add('is-show');
+        popupId.classList.add('is-show');
+    }, 100);
+}
+
+function popClose(el) {
+    const popup = el.target.parentNode;
+    const popDimmed = document.querySelector('.popup-dimmed');
+
+    popDimmed.classList.remove('is-show');
+    popup.classList.remove('is-show');
+    setTimeout(() => {
+        popDimmed.remove();
+    }, 500);
+}
+
+// load
+window.addEventListener('load', () => {
+    const scrollContent = document.querySelector('.container');
+    scrollContent.addEventListener('scroll', () => {
+        scrollIndicator();
+    });
+
     setScreenHeight();
     navBottom();
     isNow();
+    mobileNav();
+    mobileNavMenu();
 });
-
-// scroll
-window.addEventListener('scroll', () => {});
 
 // resize
 window.addEventListener('resize', () => {
@@ -86,7 +159,7 @@ window.addEventListener('touchend', () => {
 });
 
 // mousemove
-window.addEventListener('mousemove', e => {
+window.addEventListener('mousemove', () => {
     // const headerBottom = document.querySelector('#header .line-bottom > span');
     // const mouseX = e.clientX - 50;
     // headerBottom.style.transform = `translateX(${mouseX}px)`;
